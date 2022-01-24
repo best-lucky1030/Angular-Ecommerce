ad7b887089daa9061d86ffe98fdaa36afd6e9898
best-lucky1030 <bestlucky1030@gmail.com>
2022-01-24 22:44:53 +0300

    update backend

--- backend/bin/www.js
+++ backend/bin/www.js
@@ -1,37 +1,37 @@
-#!/user/bin/env node
+#!/usr/bin/env node
 
 /**
- * Module dependencies.
- */
+  * Module dependencies.
+*/
 
 var app = require('../app');
 var debug = require('debug')('backend:server');
 var http = require('http');
 
 /**
- * Get port from environment and store in Express
- */
+  * Get port from environment and store in Express.
+*/
 
 var port = normalizePort(process.env.PORT || '3000');
 app.set('port', port);
 
 /**
- * Create HTTP server.
- */
+  * Create HTTP server.
+*/
 
 var server = http.createServer(app);
 
 /**
- * Listen on provided port, on all network interfaces.
- */
+  * Listen on provided port, on all network interfaces.
+*/
 
 server.listen(port);
 server.on('error', onError);
 server.on('listening', onListening);
 
 /**
- * Normalize a port into a number, string, or false.
- */
+  * Normalize a port into a number, string, or false.
+*/
 
 function normalizePort(val) {
   var port = parseInt(val, 10);
@@ -50,8 +50,8 @@ function normalizePort(val) {
 }
 
 /**
- * Event listener for HTTP server "error" event.
- */
+  * Event listener for HTTP server "error" event.
+*/
 
 function onError(error) {
   if (error.syscall !== 'listen') {
@@ -65,7 +65,7 @@ function onError(error) {
   // handle specific listen errors with friendly messages
   switch (error.code) {
     case 'EACCES':
-      console.error(bind + ' requires elevated prvileges');
+      console.error(bind + ' requires elevated privileges');
       process.exit(1);
       break;
     case 'EADDRINUSE':
@@ -78,12 +78,12 @@ function onError(error) {
 }
 
 /**
- * Event listener for HTTP server "listening" event
- */
+  * Event listener for HTTP server "listening" event.
+*/
 
 function onListening() {
   var addr = server.address();
-  var bind  = typeof addr === 'string'
+  var bind = typeof addr === 'string'
     ? 'pipe ' + addr
     : 'port ' + addr.port;
   debug('Listening on ' + bind);
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,9 @@
 var express = require("express");
 var router = express.Router();
 
-// GET Home Page
-router.get("/", function(req, res, next) {
-  res.send({message: "Welcome to express server"});
+/* GET home page. */
+router.get("/", function (req, res, next) {
+  res.send({ message: "Welcome to express server" });
 });
 
 module.exports = router;
