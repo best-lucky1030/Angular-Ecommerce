244f4c9887091f8d4ede67b73f5ea2edfc549f6e
best-lucky1030 <bestlucky1030@gmail.com>
2022-01-26 20:32:49 +0300

    restructure & db connection

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,8 @@
-var express = require("express");
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
 
-/* GET home page. */
-router.get("/", function (req, res, next) {
-  res.send({ message: "Welcome to express server" });
-});
+const userRoute = require("./users");
+
+router.use("/api/v1/users", userRoute);
 
 module.exports = router;
